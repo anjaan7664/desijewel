6bd91741ea54bda47dcf5063b05d2e16799681c1
Mukesh <mukeshsoni73362@gmail.com>
2022-06-30 00:42:39 +0530

    converted all js to ts

--- pages/api/designs/[design].ts
+++ pages/api/designs/[design].ts
@@ -1,21 +1,22 @@
 import connectMongo from "@/utils/connectMongo";
 import Design from "@/models/design.model";
 
-/**
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- */
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     await connectMongo();
     const queryImage = req.query.design;
     const design = await Design.findOne({ image: queryImage });
-console.log('image rendered');
+    console.log("image rendered");
     res.json(design);
-    res.end()
+    res.end();
   } catch (error) {
     console.log(error);
     res.json({ error });
-    res.end()
+    res.end();
   }
 }
